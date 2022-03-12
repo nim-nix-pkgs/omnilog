a54afe3e582e60a5478db6eaaa7b391ea9af5d93
hugosenari <hugosenari@gmail.com>
2022-03-12 02:16:41 -0300

    chore: re index omnilog

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Advanced logging library for Nim with structured logging, formatters, filters and writers.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."omnilog-master".url = "path:./master";
+  inputs."omnilog-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."omnilog-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Advanced logging library for Nim with structured logging, formatters, filters and writers.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-omnilog-master.flake = false;
+  inputs.src-omnilog-master.owner = "nim-appkit";
+  inputs.src-omnilog-master.ref   = "refs/heads/master";
+  inputs.src-omnilog-master.repo  = "omnilog";
+  inputs.src-omnilog-master.type  = "github";
+  
+  inputs."values".dir   = "nimpkgs/v/values";
+  inputs."values".owner = "riinr";
+  inputs."values".ref   = "flake-pinning";
+  inputs."values".repo  = "flake-nimble";
+  inputs."values".type  = "github";
+  inputs."values".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."values".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-omnilog-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-omnilog-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
